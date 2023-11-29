c5b64ff35388aa7c2c954c3332968a116ea303df
Victor Ciolac <victor.bcfraga@gmail.com>
2023-11-29 17:16:19 -0300

    Req. 4

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,33 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  private readonly _name: string;
+  private readonly _special: number;
+  private readonly _cost: number;
+
+  constructor(name: string) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  static createdArchetypesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+
+  get name(): string {
+    return this._name;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+}
+
+export default Archetype;
